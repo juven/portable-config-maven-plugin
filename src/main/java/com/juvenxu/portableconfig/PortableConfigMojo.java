2aadb346660ec4cc06180ff0eb6c659cf242bade
Juven Xu <juvenshun@gmail.com>
2013-05-29 19:55:55 +0800

    initiate the project

---
+++ src/main/java/com/juvenxu/portableconfig/PortableConfigMojo.java
@@ -0,0 +1,17 @@
+package com.juvenxu.portableconfig;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+/**
+ * @author juven
+ */
+public class PortableConfigMojo extends AbstractMojo
+{
+  @Override
+  public void execute() throws MojoExecutionException, MojoFailureException
+  {
+    //To change body of implemented methods use File | Settings | File Templates.
+  }
+}

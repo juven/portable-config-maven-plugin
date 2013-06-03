ba8e19311e1b16ef24bc1c439f4a353a29f5b7aa
Juven Xu <juvenshun@gmail.com>
2013-06-03 19:50:33 +0800

    tests replacing 2 properties files

--- src/test/java/com/juvenxu/portableconfig/DefaultPortableConfigEngineTest.java
+++ src/test/java/com/juvenxu/portableconfig/DefaultPortableConfigEngineTest.java
@@ -5,6 +5,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.Properties;
@@ -31,20 +32,38 @@ public class DefaultPortableConfigEngineTest
   }
 
   @Test
-  public void GIVEN_portable_config_replacing_property_WHEN_run_engine_THEN_property_should_be_replaced() throws Exception
+  public void GIVEN_portable_config_replacing_properties_in_a_file_WHEN_run_engine_THEN_all_properties_should_be_replaced() throws Exception
   {
-    applyPortableConfigXml("replace_property.xml");
+    applyPortableConfigXml("replace_properties_in_a_file.xml");
 
-    Properties result = new Properties();
-    result.load(this.getClass().getResourceAsStream("/to_be_replaced/db.properties"));
+    Properties result = getResultProperties("db.properties");
     Assert.assertEquals("192.168.1.100", result.getProperty("mysql.host"));
     Assert.assertEquals("juven", result.getProperty("mysql.username"));
     Assert.assertEquals("test", result.getProperty("mysql.password"));
+  }
+
+  @Test
+  public void GIVEN_portable_config_replacing_two_properties_files_WHEN_run_engine_THEN_both_file_should_be_replaced() throws Exception
+  {
+    applyPortableConfigXml("replace_two_properties_files.xml");
+
+    Properties dbProperties = getResultProperties("db.properties");
+    Assert.assertEquals("192.168.1.100", dbProperties.getProperty("mysql.host"));
+
+    Properties serverProperties = getResultProperties("server.properties");
+    Assert.assertEquals("80", serverProperties.getProperty("server.port"));
   }
 
   private void applyPortableConfigXml(String configXml) throws PortableConfigException
   {
     InputStream portableConfigFile = this.getClass().getResourceAsStream("/portable_config/" + configXml);
     sut.apply(portableConfigFile);
+  }
+
+  private Properties getResultProperties(String propertiesFile) throws IOException
+  {
+    Properties result = new Properties();
+    result.load(this.getClass().getResourceAsStream("/to_be_replaced/" + propertiesFile));
+    return result;
   }
 }

dff4d8738bf818c0c821ea49c8de8e59b4095227
Juven Xu <juvenshun@gmail.com>
2013-06-13 15:26:20 +0800

    add assert for replacing jar

--- src/test/java/com/juvenxu/portableconfig/DefaultPortableConfigEngineReplaceJarTest.java
+++ src/test/java/com/juvenxu/portableconfig/DefaultPortableConfigEngineReplaceJarTest.java
@@ -1,14 +1,19 @@
 package com.juvenxu.portableconfig;
 
+
+import org.apache.commons.io.IOUtils;
 import org.apache.maven.monitor.logging.DefaultLog;
 import org.codehaus.plexus.logging.console.ConsoleLogger;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import javax.activation.DataSource;
 import javax.activation.FileDataSource;
-import java.io.File;
-import java.net.URL;
+import java.io.*;
+import java.util.Properties;
+import java.util.jar.JarEntry;
+import java.util.jar.JarInputStream;
 
 /**
  * @author juven
@@ -33,5 +38,53 @@ public class DefaultPortableConfigEngineReplaceJarTest
     File jar = new File(this.getClass().getResource("/to_be_replaced/" + "portableconfig-demo-1.0-SNAPSHOT.war").toURI());
 
     sut.replaceJar(potableConfigDataSource, jar);
+
+    String entryName = "WEB-INF/classes/db.properties";
+
+    Properties properties = loadJarEntryProperties(jar, entryName);
+
+    Assert.assertEquals("yup replaced", properties.getProperty("mysql.password"));
+  }
+
+  private Properties loadJarEntryProperties(File jar, String entryName) throws IOException
+  {
+    Properties properties = new Properties();
+
+    JarInputStream jarInputStream = new JarInputStream(new FileInputStream(jar));
+
+    while (true)
+    {
+      JarEntry jarEntry = jarInputStream.getNextJarEntry();
+
+      if (jarEntry == null)
+      {
+        break;
+      }
+
+      if ( jarEntry.getName().equals(entryName))
+      {
+
+        byte[] buffer = new byte[1024];
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        while (true)
+        {
+          int length = jarInputStream.read(buffer, 0, buffer.length);
+          if (length <= 0)
+          {
+            break;
+          }
+          byteArrayOutputStream.write(buffer, 0, length);
+        }
+
+        properties.load(new ByteArrayInputStream(byteArrayOutputStream.toByteArray()));
+
+
+      }
+    }
+
+    IOUtils.closeQuietly(jarInputStream);
+
+    return properties;
   }
 }

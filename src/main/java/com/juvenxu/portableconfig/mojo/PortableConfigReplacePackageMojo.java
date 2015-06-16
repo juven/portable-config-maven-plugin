cba9b3a2dfaedb186fff2c554cf3a1f74c4603ae
pengyu <pengyu@news.cn>
2015-06-16 10:40:43 +0800

    对target/classes下的资源文件也替换

--- src/main/java/com/juvenxu/portableconfig/mojo/PortableConfigReplacePackageMojo.java
+++ src/main/java/com/juvenxu/portableconfig/mojo/PortableConfigReplacePackageMojo.java
@@ -18,70 +18,69 @@ import java.io.IOException;
  * @author juven
  */
 @Mojo(
-        name = "replace-package",
-        defaultPhase = LifecyclePhase.PACKAGE
+		name = "replace-package",
+		defaultPhase = LifecyclePhase.PACKAGE
 )
-public class PortableConfigReplacePackageMojo extends AbstractMojo
-{
+public class PortableConfigReplacePackageMojo extends AbstractMojo {
 
-  @Parameter(property = "portableConfig")
-  private File portableConfig;
+	@Parameter(property = "portableConfig")
+	private File portableConfig;
 
-  @Parameter(readonly = true, defaultValue = "${project.packaging}")
-  private String packaging;
+	@Parameter(readonly = true, defaultValue = "${project.packaging}")
+	private String packaging;
 
-  @Parameter(readonly = true, defaultValue = "${project.build.directory}/${project.build.finalName}")
-  private File outputDirectory;
+	@Parameter(readonly = true, defaultValue = "${project.build.directory}/${project.build.finalName}")
+	private File outputDirectory;
 
-  @Parameter(readonly = true, defaultValue = "${project.build.directory}/${project.build.finalName}.${project.packaging}")
-  private File finalPackage;
+	@Parameter(readonly = true, defaultValue = "${project.build.directory}/${project.build.finalName}.${project.packaging}")
+	private File finalPackage;
 
-  @Component(role = PortableConfigEngine.class)
-  private PortableConfigEngine portableConfigEngine;
+	@Parameter(readonly = true, defaultValue = "${project.build.directory}/classes")
+	private File classPath;
 
-  @Override
-  public void execute() throws MojoExecutionException, MojoFailureException
-  {
+	@Component(role = PortableConfigEngine.class)
+	private PortableConfigEngine portableConfigEngine;
 
-    if (portableConfig == null || !portableConfig.exists())
-    {
-      getLog().warn("No portable config file is provided, skipping running.");
+	@Override
+	public void execute() throws MojoExecutionException, MojoFailureException {
 
-      return;
-    }
+		if (portableConfig == null || !portableConfig.exists()) {
+			getLog().warn("No portable config file is provided, skipping running.");
 
-    if (isSupported(packaging))
-    {
-      try
-      {
-        if (outputDirectory.exists() && outputDirectory.isDirectory())
-        {
-          getLog().info("Replacing: " + outputDirectory.getAbsolutePath());
+			return;
+		}
 
-          portableConfigEngine.replace(new FileDataSource(portableConfig), outputDirectory);
-        }
+		if (isSupported(packaging)) {
+			try {
+				// 替换 target/classes ，只适用于packaging=jar的时候
+				if (classPath.exists() && classPath.isDirectory()) {
+					getLog().info("Replacing: " + classPath.getAbsolutePath());
+					portableConfigEngine.replace(new FileDataSource(portableConfig), classPath);
+				}
 
-        getLog().info("Replacing: " + finalPackage.getAbsolutePath());
+				// 当war时，替换解压的那个目录
+				if (outputDirectory.exists() && outputDirectory.isDirectory()) {
+					getLog().info("Replacing: " + outputDirectory.getAbsolutePath());
 
-        portableConfigEngine.replace(new FileDataSource(portableConfig), finalPackage);
-      }
-      catch (IOException e)
-      {
-        throw new MojoExecutionException("Error while replacing package", e);
-      }
-    }
-    else
-    {
-      getLog().info(String.format("Ignoring packaging %s", packaging));
+					portableConfigEngine.replace(new FileDataSource(portableConfig), outputDirectory);
+				}
 
-      return;
-    }
+				getLog().info("Replacing: " + finalPackage.getAbsolutePath());
+
+				portableConfigEngine.replace(new FileDataSource(portableConfig), finalPackage);
+			} catch (IOException e) {
+				throw new MojoExecutionException("Error while replacing package", e);
+			}
+		} else {
+			getLog().info(String.format("Ignoring packaging %s", packaging));
+
+			return;
+		}
 
 
-  }
+	}
 
-  private boolean isSupported(String packaging)
-  {
-    return "war".equals(packaging) || "jar".equals(packaging);
-  }
+	private boolean isSupported(String packaging) {
+		return "war".equals(packaging) || "jar".equals(packaging);
+	}
 }
